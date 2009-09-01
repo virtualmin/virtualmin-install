dafaf71af0177540ec16b8c44b3bcb95d344fb07
Joe Cooper <joe@virtualmin.com>
2009-09-01 20:18:29 +0000

    version bump

--- virtualmin-install.sh
+++ virtualmin-install.sh
@@ -49,7 +49,7 @@ esac
 
 SERIAL=5556348
 KEY=ZJEOVNCMRB
-VER=1.0.2
+VER=1.0.3
 echo "$SERIAL" | grep "[^a-z^A-Z^0-9]" && echo "Serial number $SERIAL contains invalid characters." && exit
 echo "$KEY" | grep "[^a-z^A-Z^0-9]" && echo "License $KEY contains invalid characters." && exit
 
