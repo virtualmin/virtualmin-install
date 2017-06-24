184850b7bdb1d4df6675e9101af9dbe0e913a428
Joe Cooper <swelljoe@gmail.com>
2017-06-24 00:10:17 -0500

    Oops. using tags until stable release.

--- virtualmin-install.sh
+++ virtualmin-install.sh
@@ -170,7 +170,7 @@ printf "found %s\n" "$download" >> $log
 
 SERIAL=GPL
 KEY=GPL
-VER=6.0.1
+VER=6.0.0
 vm_version=6
 echo "$SERIAL" | grep "[^a-z^A-Z^0-9]" && echo "Serial number $SERIAL contains invalid characters." && exit
 echo "$KEY" | grep "[^a-z^A-Z^0-9]" && echo "License $KEY contains invalid characters." && exit
