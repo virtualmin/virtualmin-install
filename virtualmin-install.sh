ef0f9eb6ad479f1de79dd79adbe3fb4d04ffb2ae
Ilia Ross <ilia@ross.gdn>
2023-09-20 00:38:21 +0300

    Fix to add ability to use `slib.sh` from local file (if available) For development purposes

--- virtualmin-install.sh
+++ virtualmin-install.sh
@@ -277,18 +277,22 @@ pre_check_http_client() {
 }
 
 download_slib() {
-  # We need HTTP client first
-  pre_check_http_client
-
+  # If slib.sh is available locally in the same directory use it
+  if [ -f "$pwd/slib.sh" ]; then
+    chmod +x "$pwd/slib.sh"
+    . "$pwd/slib.sh"
   # Download the slib (source: http://github.com/virtualmin/slib)
-  $download "https://$upgrade_virtualmin_host/lib/slib.sh" >>"$log" 2>&1
-  if [ $? -ne 0 ]; then
-    echo "Error: Failed to download utility function library. Cannot continue. Check your network connection and DNS settings."
-    exit 1
+  else
+    # We need HTTP client first
+    pre_check_http_client
+    $download "https://$upgrade_virtualmin_host/lib/slib.sh" >>"$log" 2>&1
+    if [ $? -ne 0 ]; then
+      echo "Error: Failed to download utility function library. Cannot continue. Check your network connection and DNS settings."
+      exit 1
+    fi
+    chmod +x slib.sh
+    . ./slib.sh
   fi
-  chmod +x slib.sh
-  # shellcheck disable=SC1091
-  . ./slib.sh
 }
 
 # Utility function library
