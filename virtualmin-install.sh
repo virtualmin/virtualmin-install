33715fe0060731e660b84639dc109d98f92f105f
Ilia Ross <ilia@ross.gdn>
2024-12-23 11:19:47 +0200

    Fix to remove Debian 10 from mentions of support

--- virtualmin-install.sh
+++ virtualmin-install.sh
@@ -819,7 +819,7 @@ install_msg() {
       - Alma and Rocky 8 and 9 on x86_64
       UNSTABLERHEL
     ${CYANBG}${BLACK}${BOLD}Debian Linux and derivatives${NORMAL}${CYAN}
-      - Debian 10, 11 and 12 on i386 and amd64
+      - Debian 11 and 12 on i386 and amd64
       - Ubuntu 20.04 LTS, 22.04 LTS and 24.04 LTS on i386 and amd64${NORMAL}
       UNSTABLEDEB"
 
