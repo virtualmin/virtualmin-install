07f2c078fd68a47f29698c37005d3271270096c0
Ilia Ross <ilia@ross.gdn>
2025-12-04 00:24:46 +0200

    Fix connectivity check hosts

--- virtualmin-install.sh
+++ virtualmin-install.sh
@@ -18,6 +18,7 @@ download_virtualmin_host="${download_virtualmin_host:-download.virtualmin.com}"
 download_virtualmin_host_lib="$download_virtualmin_host"
 download_virtualmin_host_dev="${download_virtualmin_host_dev:-software.virtualmin.dev}"
 download_virtualmin_host_rc="${download_virtualmin_host_rc:-rc.software.virtualmin.dev}"
+download_webmin_host="${download_webmin_host:-download.webmin.com}"
 download_webmin_host_dev="${download_webmin_host_dev:-download.webmin.dev}"
 download_webmin_host_rc="${download_webmin_host_rc:-rc.download.webmin.dev}"
 
@@ -365,13 +366,15 @@ fi
 # If connectivity test is requested
 if [ -n "$test_connection_type" ]; then
   for test_type in $test_connection_type; do
-    test_connection "$download_virtualmin_host" "$test_type"
     if [ "$branch" = "unstable" ]; then
       test_connection "$download_webmin_host_dev" "$test_type"
       test_connection "$download_virtualmin_host_dev" "$test_type"
     elif [ "$branch" = "prerelease" ]; then
       test_connection "$download_webmin_host_rc" "$test_type"
       test_connection "$download_virtualmin_host_rc" "$test_type"
+    else
+      test_connection "$download_webmin_host" "$test_type"
+      test_connection "$download_virtualmin_host" "$test_type"
     fi
   done
   exit 0
